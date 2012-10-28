d50d63e6d173c79f4c4f11ee050af28923b430b5
Tomer Weller <tomer.weller@gmail.com>
2012-10-28 20:13:21 +0200

    a change

--- src/HelloGitMain.java
+++ src/HelloGitMain.java
@@ -6,7 +6,7 @@ public class HelloGitMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git!");
+		System.out.println("Hello Changed Git!");
 	}
 
 }
